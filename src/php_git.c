f85c6004b9ec230a9e2c6bf6886ca6e1f18bdbf9
Shuhei Tanuma <shuhei.tanuma@gmail.com>
2011-01-01 17:01:47 +0900

    fixes #57 コピーライトの変更

--- src/php_git.c
+++ src/php_git.c
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  *
- * Copyright (c) 2010 Shuhei Tanuma
+ * Copyright (c) 2010 - 2011 Shuhei Tanuma
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
