f54090730c11158d27c8022c003d4e8aef3789da
Shuhei Tanuma <shuhei.tanuma@gmail.com>
2011-01-04 22:20:28 +0900

    refs #60 added GitSignatureTest

---
+++ tests/GitSignatureTest.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * This file is part of the PECL_Git package.
+ * (c) Shuhei Tanuma
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+ 
+ class GitSignatureTest extends \PHPUnit_Framework_TestCase
+ {
+     protected function setUp()
+     {
+         // currentry nothing to do.
+     }
+     
+     protected function tearDown()
+     {
+         // currentry nothing to do.
+     }
+     
+     public function testSignatureConstruct()
+     {
+         $signature = new GitSignature("Name","sample@example.com",1294147094);
+         
+         $this->assertEquals("Name",$signature->name);
+         $this->assertEquals("sample@example.com",$signature->email);
+         $this->assertEquals(1294147094,$signature->time);
+     }
+ }
