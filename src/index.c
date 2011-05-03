9b9de59735872de58f6ecd25e62a1df58bbd0c52
Shuhei Tanuma <shuhei.tanuma@gmail.com>
2011-05-03 22:41:16 +0900

    fix variable declaration.

--- src/index.c
+++ src/index.c
@@ -29,7 +29,6 @@
 #include <time.h>
 
 PHPAPI zend_class_entry *git_index_class_entry;
-PHPAPI zend_class_entry *git_index_iterator_class_entry;
 
 static void php_git_index_free_storage(php_git_index_t *obj TSRMLS_DC)
 {
