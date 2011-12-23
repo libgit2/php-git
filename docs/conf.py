5f076e2c2cb8a842b745bda401e29900cc41dd40
Shuhei Tanuma <shuhei.tanuma@gmail.com>
2011-12-23 10:11:30 +0900

    added github extension for sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,10 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinxcontrib.phpdomain']
+extensions = ['sphinxcontrib.phpdomain','sphinxtogithub']
+
+sphinx_to_github = True
+sphinx_to_github_verbose = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
