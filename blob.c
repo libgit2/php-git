86e285d041698f0dad6bfbdd14a0b1974cf8997b
Shuhei Tanuma <chobieee@gmail.com>
2012-01-25 13:01:03 +0900

    added static method

--- blob.c
+++ blob.c
@@ -44,6 +44,12 @@ zend_object_value php_git2_blob_new(zend_class_entry *ce TSRMLS_DC)
 	return retval;
 }
 
+ZEND_BEGIN_ARG_INFO_EX(arginfo_git2_blob_create, 0,0,2)
+	ZEND_ARG_INFO(0, repository)
+	ZEND_ARG_INFO(0, contents)
+ZEND_END_ARG_INFO()
+
+
 
 /*
 {{{ proto: Git2\Blob::__toString()
@@ -67,9 +73,43 @@ PHP_METHOD(git2_blob, __toString)
 }
 /* }}} */
 
+/*
+{{{ proto: Git2\Blob::create(Git2\Repository $repository, string $contents)
+*/
+PHP_METHOD(git2_blob, create)
+{
+	char *contents;
+	int contents_len, error = 0;
+	zval *repository;
+	php_git2_repository *m_repository;
+	char oid_out[GIT_OID_HEXSZ] = {0};
+	git_oid oid;
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC,
+	"Os", &repository, git2_repository_class_entry, &contents, &contents_len) == FAILURE) {
+		return;
+	}
+	
+	m_repository = PHP_GIT2_GET_OBJECT(php_git2_repository, repository);
+
+	if (m_repository != NULL) {
+		if (m_repository->repository == NULL) {
+			RETURN_FALSE;
+		}
+		
+		error = git_blob_create_frombuffer(&oid, m_repository->repository, contents, contents_len);
+		git_oid_fmt(oid_out, &oid);
+		RETVAL_STRINGL(oid_out,GIT_OID_HEXSZ,1);
+	} else {
+		RETURN_FALSE;
+	}
+}
+/* }}} */
+
 
 static zend_function_entry php_git2_blob_methods[] = {
-	PHP_ME(git2_blob, __toString, NULL, ZEND_ACC_PUBLIC)
+	PHP_ME(git2_blob, __toString, NULL,                     ZEND_ACC_PUBLIC)
+	PHP_ME(git2_blob, create,     arginfo_git2_blob_create, ZEND_ACC_PUBLIC | ZEND_ACC_STATIC)
 	{NULL,NULL,NULL}
 };
 
