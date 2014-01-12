18fc8310ea35c9d390bf97b1a1a198765c94d886
Shuhei Tanuma <shuhei.tanuma@gmail.com>
2014-01-12 11:56:46 +0900

    [ignore] add functions

--- ignore.c
+++ ignore.c
@@ -2,62 +2,74 @@
 #include "php_git2_priv.h"
 #include "ignore.h"
 
-/* {{{ proto long git_ignore_add_rule(repo, rules)
-*/
+/* {{{ proto long git_ignore_add_rule(resource $repo, string $rules)
+ */
 PHP_FUNCTION(git_ignore_add_rule)
 {
-	zval *repo;
-	php_git2_t *_repo;
-	char *rules = {0};
-	int rules_len;
-
-	/* TODO(chobie): implement this */
-	php_error_docref(NULL TSRMLS_CC, E_WARNING, "git_ignore_add_rule not implemented yet");
-	return;
+	int result = 0;
+	zval *repo = NULL;
+	php_git2_t *_repo = NULL;
+	char *rules = NULL;
+	int rules_len = 0;
+	int error = 0;
 
 	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC,
 		"rs", &repo, &rules, &rules_len) == FAILURE) {
 		return;
 	}
-	ZEND_FETCH_RESOURCE(_repo, php_git2_t*, &repo, -1, PHP_GIT2_RESOURCE_NAME, git2_resource_handle);
-}
 
-/* {{{ proto long git_ignore_clear_internal_rules(repo)
-*/
+	ZEND_FETCH_RESOURCE(_repo, php_git2_t*, &repo, -1, PHP_GIT2_RESOURCE_NAME, git2_resource_handle);
+	result = git_ignore_add_rule(PHP_GIT2_V(_repo, repository), rules);
+	RETURN_LONG(result);
+}
+/* }}} */
+
+
+/* {{{ proto long git_ignore_clear_internal_rules(resource $repo)
+ */
 PHP_FUNCTION(git_ignore_clear_internal_rules)
 {
-	zval *repo;
-	php_git2_t *_repo;
-
-	/* TODO(chobie): implement this */
-	php_error_docref(NULL TSRMLS_CC, E_WARNING, "git_ignore_clear_internal_rules not implemented yet");
-	return;
+	int result = 0;
+	zval *repo = NULL;
+	php_git2_t *_repo = NULL;
+	int error = 0;
 
 	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC,
 		"r", &repo) == FAILURE) {
 		return;
 	}
-	ZEND_FETCH_RESOURCE(_repo, php_git2_t*, &repo, -1, PHP_GIT2_RESOURCE_NAME, git2_resource_handle);
-}
 
-/* {{{ proto long git_ignore_path_is_ignored(ignored, repo, path)
-*/
+	ZEND_FETCH_RESOURCE(_repo, php_git2_t*, &repo, -1, PHP_GIT2_RESOURCE_NAME, git2_resource_handle);
+	result = git_ignore_clear_internal_rules(PHP_GIT2_V(_repo, repository));
+	RETURN_LONG(result);
+}
+/* }}} */
+
+
+/* {{{ proto long git_ignore_path_is_ignored(resource $repo, string $path)
+ */
 PHP_FUNCTION(git_ignore_path_is_ignored)
 {
-	long ignored;
-	zval *repo;
-	php_git2_t *_repo;
-	char *path = {0};
-	int path_len;
+	int result = 0;
+	long ignored = 0;
+	zval *repo = NULL;
+	php_git2_t *_repo = NULL;
+	char *path = NULL;
+	int path_len = 0;
+	int error = 0;
 
-	/* TODO(chobie): implement this */
-	php_error_docref(NULL TSRMLS_CC, E_WARNING, "git_ignore_path_is_ignored not implemented yet");
-	return;
-//
-//	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC,
-//		"lrs", &ignored, &repo, &path, &path_len) == FAILURE) {
-//		return;
-//	}
-//	ZEND_FETCH_RESOURCE(_ignored, php_git2_t*, &ignored, -1, PHP_GIT2_RESOURCE_NAME, git2_resource_handle);
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC,
+		"rs", &repo, &path, &path_len) == FAILURE) {
+		return;
+	}
+
+	ZEND_FETCH_RESOURCE(_repo, php_git2_t*, &repo, -1, PHP_GIT2_RESOURCE_NAME, git2_resource_handle);
+	result = git_ignore_path_is_ignored(&ignored, PHP_GIT2_V(_repo, repository), path);
+	if (php_git2_check_error(error, "git_ignore_path_is_ignored" TSRMLS_CC)) {
+		RETURN_FALSE
+	}
+
+	RETURN_LONG(ignored);
 }
+/* }}} */
 
